91b062152235bc4290f108dcb8040fb6bb54b9e0
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-02-24 15:44:16 +0200

    Delete L02_ArraysAndNestedArrays/Lab directory

--- L02_ArraysAndNestedArrays/Lab/T03SumFirstLast.js
+++
@@ -1,6 +0,0 @@
-function solve(input) {
-    return Number(input[0]) + Number(input[input.length - 1]);
-}
-
-console.log(solve(['20', '30', '40']));
-console.log(solve(['5', '10']));
--- L02_ArraysAndNestedArrays/Lab/T05SmallestTwoNumbers.js
+++
@@ -1,9 +0,0 @@
-function solve(arr) {
-    return arr
-        .sort((x, y) => x - y)
-        .slice(0, 2)
-        .join(' ');
-}
-
-console.log(solve([30, 15, 50, 5]));
-console.log(solve([3, 0, 10, 4, 7, 3]));
