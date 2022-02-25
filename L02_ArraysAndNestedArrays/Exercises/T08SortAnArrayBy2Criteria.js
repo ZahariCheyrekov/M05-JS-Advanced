1107db741ed32e721b839b1b0ce6b0b18f2e4ff4
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-02-25 17:23:48 +0200

    Add files via upload

---
+++ L02_ArraysAndNestedArrays/Exercises/T08SortAnArrayBy2Criteria.js
@@ -0,0 +1,12 @@
+function solve(arr) {
+    return arr.sort((a, b) =>
+        a.length - b.length === 0
+            ? a.toLocaleLowerCase().localeCompare(b.toLocaleLowerCase())
+            : a.length - b.length
+    )
+        .join('\n')
+}
+
+console.log(solve(['alpha', 'beta', 'gamma']));
+console.log(solve(['Isacc', 'Theodor', 'Jack', 'Harrison', 'George']));
+console.log(solve(['test', 'Deny', 'omen', 'Default']));
