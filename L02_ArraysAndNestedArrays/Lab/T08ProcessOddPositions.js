3aafec7c85973cc26ff4f1e1384e2ee33638a513
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-02-25 17:20:59 +0200

    Delete L02_ArraysAndNestedArrays/Lab directory

--- L02_ArraysAndNestedArrays/Lab/T08ProcessOddPositions.js
+++
@@ -1,8 +0,0 @@
-function solve(arr) {
-    return arr.filter((_, i) => i % 2 !== 0)
-        .map(n => n * 2)
-        .reverse();
-}
-
-console.log(solve([10, 15, 20, 25]));
-console.log(solve([3, 0, 10, 4, 7, 3]));
