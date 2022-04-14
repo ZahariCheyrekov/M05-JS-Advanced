9690e9278d16beb343b1007f4d965ec609fa05a3
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-14 12:42:35 +0300

    Update T05AssemblyLine.js

--- L03_ObjectsAndComposition/Lab/T05AssemblyLine.js
+++ L03_ObjectsAndComposition/Lab/T05AssemblyLine.js
@@ -27,7 +27,6 @@ function createAssemblyLine() {
 
         hasParktronic: (car) => {
             car.checkDistance = (distance) => {
-           
                 if (distance < 0.1) {
                     console.log('Beep! Beep! Beep!');
                 } else if (distance >= 0.1 && distance < 0.25) {
