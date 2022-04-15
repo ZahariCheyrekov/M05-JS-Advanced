111c9689a6480e6e452a0abe31e5cd1f0d8ce06b
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-15 16:08:33 +0300

    Add files via upload

---
+++ L03_ObjectsAndComposition/Exercises/T08Retangle.js
@@ -0,0 +1,18 @@
+function solve(width, height, color) {
+    const rectangle = {
+        width,
+        height,
+        color: color[0].toUpperCase() + color.slice(1, color.length ),
+        calcArea() {
+            return this.width * this.height;
+        }
+    }
+
+    return rectangle;
+}
+
+let rect = solve(4, 5, 'red');
+console.log(rect.width);
+console.log(rect.height);
+console.log(rect.color);
+console.log(rect.calcArea());
