9ec7a662bf2953cc39bf85197a25064a23037efd
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-17 10:35:03 +0300

    Add files via upload

---
+++ L04_DOM/Exercises/04. Search in List/search.js
@@ -0,0 +1,31 @@
+function search() {
+   const listItems = Array.from(document.querySelectorAll('ul li'));
+   const text = document.getElementById('searchText').value;
+
+   let resultsFound = 0;
+
+   listItems.forEach((el) => {
+      el.style.fontWeight = 'normal';
+      el.style.textDecoration = '';
+      document.getElementById('result').textContent = '';
+   });
+
+   if (text.trim() == '') {
+      el.style.fontWeight = 'normal';
+      el.style.textDecoration = '';
+   } else {
+      listItems.forEach((el) => {
+
+         if (el.innerHTML.includes(text)) {
+            el.style.fontWeight = 'bold';
+            el.style.textDecoration = 'underline';
+            resultsFound++;
+         } else {
+            el.style.fontWeight = 'normal';
+            el.style.textDecoration = '';
+         }
+      });
+   }
+
+   document.getElementById('result').textContent = `${resultsFound} matches found`;
+}
