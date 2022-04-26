49b72194224875182b4132a3a11ddeac38242c9a
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-26 21:40:44 +0300

    Update T05LengthLimit.js

--- L08_Classes/Exercises/T05LengthLimit.js
+++ L08_Classes/Exercises/T05LengthLimit.js
@@ -22,7 +22,7 @@ class Stringer {
 }
 
 let test = new Stringer("Test", 5);
-console.log(test.toString()); // Test
+console.log(test.toString());
 
 test.decrease(3);
 console.log(test.toString()); // Te...
