a68db1d319d527b6d98661f1ff3bf153beb3db14
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-05-14 22:55:46 +0300

    Delete T03LargestNumber.js

--- L01_SyntaxFunctionsAndStatements/Lab/T03LargestNumber.js
+++
@@ -1,7 +0,0 @@
-function largestNumber(a, b, c) {
-    const largest = Math.max(a, b, c);
-    console.log(`The largest number is ${largest}.`);
-}
-
-largestNumber(5, -3, 16);
-largestNumber(-3, -5, -22.5);
