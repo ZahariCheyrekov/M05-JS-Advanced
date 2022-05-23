0f29b8dcebcaf0627ce408dd9ef42eb02e0c1620
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-05-23 19:37:19 +0300

    Update T09MagicMatrices.js

--- L02_ArraysAndNestedArrays/Exercises/T09MagicMatrices.js
+++ L02_ArraysAndNestedArrays/Exercises/T09MagicMatrices.js
@@ -1,10 +1,8 @@
 function solve(arr) {
-
     let sumRow = arr.map((col => col.reduce((a, b) => a + b)));
     let sumCol = arr.reduce((r, a) => r.map((b, i) => a[i] + b));
 
     let areEqual = array => array.every(v => v === array[0]);
-
     
     return areEqual(sumRow) && areEqual(sumRow) && sumRow.toString() === sumCol.toString();
 }
