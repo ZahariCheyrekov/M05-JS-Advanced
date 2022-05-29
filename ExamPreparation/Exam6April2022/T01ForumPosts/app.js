0a7d25bb0cc7b02e59147678980fa9cc07986971
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-29 10:29:06 +0300

    Get fields from the html

--- ExamPreparation/Exam6April2022/T01ForumPosts/app.js
+++ ExamPreparation/Exam6April2022/T01ForumPosts/app.js
@@ -1,5 +1,9 @@
 window.addEventListener("load", solve);
 
 function solve() {
-  //TODO ...
+  const titleField = document.getElementById('post-title');
+  const categoryField = document.getElementById('post-category');
+  const contentField = document.getElementById('post-content');
+  
+  console.log(titleField, categoryField, contentField);
 }
