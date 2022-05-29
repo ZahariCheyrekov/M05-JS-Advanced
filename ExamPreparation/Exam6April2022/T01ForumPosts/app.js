910c96107c77dfe6772e56fd2b66b71585f22e5d
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-29 10:56:39 +0300

    Add li content

--- ExamPreparation/Exam6April2022/T01ForumPosts/app.js
+++ ExamPreparation/Exam6April2022/T01ForumPosts/app.js
@@ -5,14 +5,51 @@ function solve() {
   const categoryField = document.getElementById('post-category');
   const contentField = document.getElementById('post-content');
   const publishBtn = document.getElementById('publish-btn');
+  const ulEl = document.getElementById('review-list');
   publishBtn.addEventListener('click', getInformation);
 
   function getInformation() {
-    if (!titleField.value || !categoryField.value || !contentField.value) {
+    const titleText = titleField.value;
+    const categoryText = categoryField.value;
+    const contentText = contentField.value;
+
+    if (!titleText || !categoryText || !contentText) {
       return;
     }
 
-    const ulItem = document.createElement('ul');
+    const liItem = document.createElement('li');
+    liItem.classList.add('rpost');
+
+    const articleEl = document.createElement('article');
+
+    const titlePost = document.createElement('h4');
+    titlePost.textContent = titleField.value;
+
+    const categoryPost = document.createElement('p');
+    categoryPost.textContent = `Category: ${categoryField.value}`;
+
+    const contentPost = document.createElement('p');
+    contentPost.textContent = `Content: ${contentField.value}`;
+
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('action-btn');
+    editBtn.classList.add('edit');
+    editBtn.textContent = 'Edit';
+
+    const approveBtn = document.createElement('button');
+    approveBtn.classList.add('action-btn');
+    approveBtn.classList.add('approve');
+    approveBtn.textContent = 'Approve';
+
+    articleEl.appendChild(titlePost);
+    articleEl.appendChild(categoryPost);
+    articleEl.appendChild(contentPost);
+
+    liItem.appendChild(articleEl);
+    liItem.appendChild(editBtn);
+    liItem.appendChild(approveBtn);
+
+    ulEl.appendChild(liItem);
 
     clearInputFields();
   }
