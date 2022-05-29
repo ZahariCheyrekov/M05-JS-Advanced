bd1720e265687bdc30769a988fb9e9ef4b2d3cb1
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-29 10:30:15 +0300

    Get publish button

--- ExamPreparation/Exam6April2022/T01ForumPosts/app.js
+++ ExamPreparation/Exam6April2022/T01ForumPosts/app.js
@@ -4,6 +4,5 @@ function solve() {
   const titleField = document.getElementById('post-title');
   const categoryField = document.getElementById('post-category');
   const contentField = document.getElementById('post-content');
-  
-  console.log(titleField, categoryField, contentField);
+  const publishBtn = document.getElementById('publish-btn');
 }
