99fe0a8fb03896b3938c80f5d22012e07695efc3
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-29 13:36:15 +0300

    Add suitable books test

--- ExamPreparation/Exam6April2022/T03BookSelection/bookSelection.test.js
+++ ExamPreparation/Exam6April2022/T03BookSelection/bookSelection.test.js
@@ -1,3 +1,30 @@
 const { assert } = require('chai');
 const bookSelection = require('./bookSelection');
 
+describe('BookSelection Tests', () => {
+    describe('Tests should check if the genre is suitable', () => {
+        it('should return not suitable books for kids at the age of 12', () => {
+            const expectedMessage = 'Books with Thriller genre are not suitable for kids at 12 age';
+            const actualMessage = bookSelection.isGenreSuitable('Thriller', 12);
+            assert.equal(expectedMessage, actualMessage);
+        });
+
+        it('should return not suitable books for kids at the age of less than 13', () => {
+            const expectedMessage = 'Books with Horror genre are not suitable for kids at 5 age';
+            const actualMessage = bookSelection.isGenreSuitable('Horror', 5);
+            assert.equal(expectedMessage, actualMessage);
+        });
+
+        it('should return that books are suitable for kids at the age of 15', () => {
+            const expectedMessage = 'Those books are suitable';
+            const actualMessage = bookSelection.isGenreSuitable('Horror', 15);
+            assert.equal(expectedMessage, actualMessage);
+        });
+
+        it('should return that books are suitable for kids at the age of 18', () => {
+            const expectedMessage = 'Those books are suitable';
+            const actualMessage = bookSelection.isGenreSuitable('Thriller', 18);
+            assert.equal(expectedMessage, actualMessage);
+        });
+    });
+});
