0d5fb19a651cd6583f07d1cfae30ef3b0f1901df
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-29 12:08:04 +0300

    Add Garden ripenPlant function

--- ExamPreparation/Exam6April2022/T02Garden/Garden.js
+++ ExamPreparation/Exam6April2022/T02Garden/Garden.js
@@ -14,4 +14,29 @@ class Garden {
             return `The ${plantName} has been successfully planted in the garden.`;
         }
     }
+
+    ripenPlant(plantName, quantity) {
+        const plant = this.plants.some(plant => plant.plantName == plantName);
+
+        if (!plant) {
+            throw new Error(`There is no ${plantName} in the garden.`);
+        }
+
+        if (plant.ripe) {
+            throw new Error(`The ${plantName} is already ripe.`);
+        }
+
+        if (quantity <= 0) {
+            throw new Error('The quantity cannot be zero or negative.');
+        }
+
+        plant.ripe = true;
+        plant.quantity += quantity;
+
+        const message = quantity == 1
+            ? `${quantity} ${plantName} has successfully ripened.`
+            : `${quantity} ${plantName}s has successfully ripened.`;
+
+        return message;
+    }
 }
