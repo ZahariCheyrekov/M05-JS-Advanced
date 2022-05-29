3ae005d69b4b0411ea043ae49ea8b94676f8184b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-29 14:03:52 +0300

    Add tests for purchase

--- ExamPreparation/Exam6April2022/T03BookSelection/bookSelection.test.js
+++ ExamPreparation/Exam6April2022/T03BookSelection/bookSelection.test.js
@@ -1,4 +1,4 @@
-const { assert } = require('chai');
+const { assert, expect } = require('chai');
 const bookSelection = require('./bookSelection');
 
 describe('BookSelection Tests', () => {
@@ -24,6 +24,32 @@ describe('BookSelection Tests', () => {
         it('should return that books are suitable for kids at the age of 18', () => {
             const expectedMessage = 'Those books are suitable';
             const actualMessage = bookSelection.isGenreSuitable('Thriller', 18);
+            assert.equal(expectedMessage, actualMessage);
+        });
+    });
+
+    describe('Tests should check if purchase is affordable', () => {
+        it('should trow error for type of price different than number', () => {
+            expect(() => bookSelection.isItAffordable('NaN', 10)).to.throw(Error);
+        });
+
+        it('should trow error for type of budget different than number', () => {
+            expect(() => bookSelection.isItAffordable(10, 'NaN')).to.throw(Error);
+        });
+
+        it('should trow error different type of number values', () => {
+            expect(() => bookSelection.isItAffordable('abc', 'def')).to.throw(Error);
+        });
+
+        it('should return that user don\'t have enough money', () => {
+            const expectedMessage = 'You don\'t have enough money';
+            const actualMessage = bookSelection.isItAffordable(20, 10);
+            assert.equal(expectedMessage, actualMessage);
+        });
+
+        it('should return that user bought the book successfully', () => {
+            const expectedMessage = 'Book bought. You have 10$ left';
+            const actualMessage = bookSelection.isItAffordable(10, 20);
             assert.equal(expectedMessage, actualMessage);
         });
     });
