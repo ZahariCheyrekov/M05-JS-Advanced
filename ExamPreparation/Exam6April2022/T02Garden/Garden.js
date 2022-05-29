86a2977b2e323ac2f319f2c0e0b00ec3c8123a11
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-29 12:01:43 +0300

    Add Garden addPlant function

--- ExamPreparation/Exam6April2022/T02Garden/Garden.js
+++ ExamPreparation/Exam6April2022/T02Garden/Garden.js
@@ -4,4 +4,14 @@ class Garden {
         this.plants = [];
         this.storage = [];
     }
+
+    addPlant(plantName, spaceRequired) {
+        if (this.spaceAvailable < spaceRequired) {
+            throw new Error('Not enough space in the garden.');
+        } else {
+            this.plants.push({ plantName, spaceRequired, ripe: false, quantity: 0 });
+            this.spaceAvailable -= spaceRequired;
+            return `The ${plantName} has been successfully planted in the garden.`;
+        }
+    }
 }
