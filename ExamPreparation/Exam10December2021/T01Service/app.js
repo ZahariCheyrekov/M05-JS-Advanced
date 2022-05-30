fdd4f034c865c4a76a0fd4d02ae3723310ca5a91
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-30 11:52:59 +0300

    Add DOM elements

--- ExamPreparation/Exam10December2021/T01Service/app.js
+++ ExamPreparation/Exam10December2021/T01Service/app.js
@@ -1,5 +1,12 @@
 window.addEventListener('load', solve);
 
 function solve() {
-  
+    const productType = document.getElementById('type-product');
+    const description = document.getElementById('description');
+    const text = document.getElementById('client-name');
+    const phone = document.getElementById('client-phone');
+    const sendBtn = document.querySelector('form button');
+    sendBtn.addEventListener('click', sendOrder);
+
+    
 }
