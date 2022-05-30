765a118490255b2b0f352cd0171d06a8dc31d32e
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-30 09:44:27 +0300

    Add removeBook function

--- ExamPreparation/Exam19February2022/T02LibraryCollection/libraryCollection.js
+++ ExamPreparation/Exam19February2022/T02LibraryCollection/libraryCollection.js
@@ -34,6 +34,23 @@ class LibraryCollection {
         return `${bookName} has been successfully paid.`;
     }
 
+    removeBook(bookName) {
+        const book = this.findBookByName(bookName);
+
+        if (!book) {
+            throw new Error('The book, you\'re looking for, is not found.');
+        }
+
+        if (!book.payed) {
+            throw new Error(`${bookName} need to be paid before removing from the collection.`);
+        }
+
+        const bookIndex = this.books.indexOf(book);
+        this.books.splice(bookIndex, 1);
+
+        return `${bookName} remove from the collection.`;
+    }
+
     findBookByName(name) {
         return this.books.find(b => b.bookName == name);
     }
