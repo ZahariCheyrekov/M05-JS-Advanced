4fc390c88c2d9ebd8a3cdff67e7646ba68f8f3fd
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-30 08:43:18 +0300

    Add tests for invalit input values

--- ExamPreparation/Exam13March2022/T03RentCar/rentCar.test.js
+++ ExamPreparation/Exam13March2022/T03RentCar/rentCar.test.js
@@ -2,5 +2,33 @@ const { assert, expect } = require('chai');
 const rentCar = require('./rentCar');
 
 describe('RentCar Tests', () => {
-  
+    describe('Tests should check budget for rentig a car', () => {
+        it('should throw error for not integer value for the costPerDay', () => {
+            expect(() => rentCar.checkBudget('a', 10, 10)).to.throw(Error);
+        });
+
+        it('should throw error for not integer value for the days', () => {
+            expect(() => rentCar.checkBudget(10, 'a', 10)).to.throw(Error);
+        });
+
+        it('should throw error for not integer value for the budget', () => {
+            expect(() => rentCar.checkBudget(10, 10, 'a')).to.throw(Error);
+        });
+
+        it('should throw error for not integer value for the costPerDay and not integer value for the days', () => {
+            expect(() => rentCar.checkBudget('a', 'a', 10)).to.throw(Error);
+        });
+
+        it('should throw error for not integer value for the days and not integer value for the budget', () => {
+            expect(() => rentCar.checkBudget(10, 'a', 'a')).to.throw(Error);
+        });
+
+        it('should throw error for not integer value for the costPerDay and not integer value for the budget', () => {
+            expect(() => rentCar.checkBudget('a', 10, 'a')).to.throw(Error);
+        });
+
+        it('should throw error for not integer values', () => {
+            expect(() => rentCar.checkBudget('a', 'a', 'a')).to.throw(Error);
+        });
+    });
 });
