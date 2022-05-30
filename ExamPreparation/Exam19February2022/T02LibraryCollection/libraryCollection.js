abede7882bcfee7a583ff5d3308302c87ae4225c
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-30 09:37:25 +0300

    Add LibraryCollection addBook function

--- ExamPreparation/Exam19February2022/T02LibraryCollection/libraryCollection.js
+++ ExamPreparation/Exam19February2022/T02LibraryCollection/libraryCollection.js
@@ -3,4 +3,19 @@ class LibraryCollection {
         this.capacity = capacity;
         this.books = [];
     }
+
+    addBook(bookName, bookAuthor) {
+        if (this.capacity <= this.books.length) {
+            throw new Error('Not enough space in the collection.');
+        }
+
+        const book = {
+            bookName,
+            bookAuthor,
+            payed: false
+        };
+
+        this.books.push(book);
+        return `The ${bookName}, with an author ${bookAuthor}, collect.`;
+    }
 }
