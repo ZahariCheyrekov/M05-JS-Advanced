22e10e6196744db30f4c2de5731cfe3a23fbc8b4
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-30 09:15:22 +0300

    Add tr element to the tbody with content

--- ExamPreparation/Exam19February2022/T01WorkProcess/app.js
+++ ExamPreparation/Exam19February2022/T01WorkProcess/app.js
@@ -9,6 +9,8 @@ function solve() {
     hireWorkerBtn.type = 'button';
     hireWorkerBtn.addEventListener('click', hireWorker);
 
+    const tbody = document.getElementById('tbody');
+
     function hireWorker() {
         const firstName = firstNameField.value
         const lastName = lastNameField.value;
@@ -20,6 +22,47 @@ function solve() {
         if (!firstName || !lastName || !email || !birth || !position || !salary) {
             return;
         }
+
+        const tr = document.createElement('tr');
+        const firstNameText = document.createElement('td');
+        firstNameText.textContent = firstName;
+
+        const lastNameText = document.createElement('td');
+        lastNameText.textContent = lastName;
+
+        const emailText = document.createElement('td');
+        emailText.textContent = email;
+
+        const birthText = document.createElement('td');
+        birthText.textContent = birth;
+
+        const positionText = document.createElement('td');
+        positionText.textContent = position;
+
+        const salaryNum = document.createElement('td');
+        salaryNum.textContent = salary;
+
+        const trButtons = document.createElement('td');
+        const firedButton = document.createElement('button');
+        firedButton.textContent = 'Fired';
+        firedButton.classList.add('fired');
+
+        const editButton = document.createElement('button');
+        editButton.textContent = 'Edit';
+        editButton.classList.add('Edit');
+
+        trButtons.appendChild(firedButton);
+        trButtons.appendChild(editButton);
+
+        tr.appendChild(firstNameText);
+        tr.appendChild(lastNameText);
+        tr.appendChild(emailText);
+        tr.appendChild(birthText);
+        tr.appendChild(positionText);
+        tr.appendChild(salaryNum);
+        tr.appendChild(trButtons);
+
+        tbody.appendChild(tr);
 
         clearInputFields();
     }
