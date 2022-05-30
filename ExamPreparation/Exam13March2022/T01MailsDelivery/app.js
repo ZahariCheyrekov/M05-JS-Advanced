3b47d1b94a027bf8a34d93839cfe43697dde83a4
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-30 08:40:23 +0300

    Add submit button logic

--- ExamPreparation/Exam13March2022/T01MailsDelivery/app.js
+++ ExamPreparation/Exam13March2022/T01MailsDelivery/app.js
@@ -42,6 +42,44 @@ function solve() {
         submitBtn.type = 'submit';
         submitBtn.id = 'send';
         submitBtn.textContent = 'Send';
+
+        submitBtn.addEventListener('click', (ev) => {
+            elementToRemove = ev.target.parentElement.parentElement;
+            listOfMails.removeChild(elementToRemove);
+            divEl.removeChild(submitBtn);
+
+            const title = document.createElement('span');
+            title.textContent = `Title: ${titleContent}`;
+
+            const recipient = document.createElement('span');
+            recipient.textContent = `To: ${recipentContent}`;
+
+            const div = document.createElement('div');
+            div.classList.add('btn');
+
+            const button = document.createElement('button');
+            button.addEventListener('click', (ev) => {
+                elementToRemove = ev.target.parentElement.parentElement;
+                elementToRemove.removeChild(div);
+                deletedMails.appendChild(elementToRemove);
+            });
+
+            button.type = 'submit';
+            button.classList.add('delete');
+            button.textContent = 'Delete';
+            div.appendChild(button);
+
+            liEl.removeChild(titleEl);
+            liEl.removeChild(recipentName);
+            liEl.removeChild(messageEl);
+            liEl.removeChild(divEl);
+
+            liEl.appendChild(recipient);
+            liEl.appendChild(title);
+            liEl.appendChild(div);
+
+            sendMails.appendChild(elementToRemove);
+        });
     }
 
     function resetFieldValues() {
