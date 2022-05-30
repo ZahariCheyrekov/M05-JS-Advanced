78c7b7d0fbf4be3e988398a316425b91e1714bb6
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-30 12:04:53 +0300

    Add elements for the repair section

--- ExamPreparation/Exam10December2021/T01Service/app.js
+++ ExamPreparation/Exam10December2021/T01Service/app.js
@@ -3,29 +3,51 @@ window.addEventListener('load', solve);
 function solve() {
     const productType = document.getElementById('type-product');
     const description = document.getElementById('description');
-    const text = document.getElementById('client-name');
+    const name = document.getElementById('client-name');
     const phone = document.getElementById('client-phone');
     const sendBtn = document.querySelector('form button');
     sendBtn.addEventListener('click', sendOrder);
+
+    const recievedOrdersSection = document.getElementById('received-orders');
 
     function sendOrder(ev) {
         ev.preventDefault();
 
         const productTypeContent = productType.value;
         const descriptionContent = description.value;
-        const textContent = text.value;
+        const nameContent = name.value;
         const phoneContent = phone.value;
 
-        if (!descriptionContent || !textContent || !phoneContent) {
+        if (!descriptionContent || !nameContent || !phoneContent) {
             return;
         }
+
+        const container = document.createElement('div');
+        container.classList.add('container');
+
+        const productTypeRepair = document.createElement('h2');
+        productTypeRepair.textContent = `Product type for repair: ${productTypeContent}`;
+
+        const clientInformation = document.createElement('h3');
+        clientInformation.textContent = `Client information: ${nameContent}, ${phoneContent}`;
+
+        const description = document.createElement('h4');
+        description.textContent = `Description of the problem: ${descriptionContent}`;
+
+        const startRepairBtn = document.createElement('button');
+        startRepairBtn.textContent = 'Start repair';
+        startRepairBtn.classList.add('start-btn');
+
+        const finishRepairBtn = document.createElement('button');
+        finishRepairBtn.textContent = 'Finish repair';
+        finishRepairBtn.classList.add('finish-btn');
 
         clearInputFields();
     }
 
     function clearInputFields() {
         description.value = '';
-        text.value = '';
+        name.value = '';
         phone.value = '';
     }
 }
