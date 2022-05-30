dbb47cac5f2905283a6d3042e808a89e6a92c347
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-30 13:24:23 +0300

    Add buyingVegetables function

--- ExamPreparation/Exam10December2021/T02VegetableStore/vegetableStore.js
+++ ExamPreparation/Exam10December2021/T02VegetableStore/vegetableStore.js
@@ -33,6 +33,30 @@ class VegetableStore {
             }
         }
 
-        return `Successfully added ${result.join(', ')}`
+        return `Successfully added ${result.join(', ')}`;
+    }
+
+    buyingVegetables(selectedProducts) {
+        let totalPrice = 0.00;
+
+        for (const element of selectedProducts) {
+            let [type, quantity] = element.split(' ');
+
+            const existingVegetable = this.availableProducts.find(p => p.type == type);
+
+            if (!existingVegetable) {
+                throw new Error(`${type} is not available in the store, your current bill is $${totalPrice.toFixed(2)}.`);
+
+            } else if (quantity > existingVegetable.quantity) {
+                throw new Error(`The quantity ${quantity} for the vegetable ${type} is not available in the store, your current bill is $${totalPrice.toFixed(2)}.`);
+
+            } else {
+                const price = quantity * existingVegetable.price;
+                existingVegetable.quantity -= quantity;
+                totalPrice += price;
+            }
+        }
+
+        return `Great choice! You must pay the following amount $${totalPrice.toFixed(2)}.`;
     }
 }
