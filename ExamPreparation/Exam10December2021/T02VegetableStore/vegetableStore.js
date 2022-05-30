3e05ff4bb2a20b3723023768f8ea79aa24802547
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-30 13:40:46 +0300

    Add input data for testing

--- ExamPreparation/Exam10December2021/T02VegetableStore/vegetableStore.js
+++ ExamPreparation/Exam10December2021/T02VegetableStore/vegetableStore.js
@@ -92,3 +92,10 @@ class VegetableStore {
         return this.availableProducts.find(v => v.type == type);
     }
 }
+
+let vegStore = new VegetableStore("Jerrie Munro", "1463 Pette Kyosheta, Sofia");
+console.log(vegStore.loadingVegetables(["Okra 2.5 3.5", "Beans 10 2.8", "Celery 5.5 2.2", "Celery 0.5 2.5"]));
+console.log(vegStore.rottingVegetable("Okra", 1));
+console.log(vegStore.rottingVegetable("Okra", 2.5));
+console.log(vegStore.buyingVegetables(["Beans 8", "Celery 1.5"]));
+console.log(vegStore.revision());
