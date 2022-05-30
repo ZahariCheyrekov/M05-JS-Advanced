0c645ddafa4f7c30fb3004f0e6863a09941836a1
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-30 12:59:42 +0300

    Add loadingVegetables function

--- ExamPreparation/Exam10December2021/T02VegetableStore/vegetableStore.js
+++ ExamPreparation/Exam10December2021/T02VegetableStore/vegetableStore.js
@@ -5,5 +5,34 @@ class VegetableStore {
         this.availableProducts = [];
     }
 
-  
+    loadingVegetables(vegetables) {
+        let result = [];
+
+        for (const element of vegetables) {
+            let [type, quantity, price] = element.split(' ');
+            quantity = Number(quantity);
+            price = Number(price);
+
+            const existingVegetable = this.availableProducts.find(p => p.type == type);
+
+            if (existingVegetable) {
+                existingVegetable.price += price;
+
+                if (existingVegetable.quantity < quantity) {
+                    existingVegetable.quantity = quantity;
+                }
+            } else {
+                const vegetable = {
+                    type,
+                    quantity,
+                    price
+                };
+
+                this.availableProducts.push(vegetable);
+                result.push(type);
+            }
+        }
+
+        return `Successfully added ${result.join(', ')}`
+    }
 }
