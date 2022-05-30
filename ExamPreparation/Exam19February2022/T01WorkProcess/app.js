610283019c91b4936e6b1c51d5ba8e283864a833
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-30 08:59:00 +0300

    Add input fields from the DOM

--- ExamPreparation/Exam19February2022/T01WorkProcess/app.js
+++ ExamPreparation/Exam19February2022/T01WorkProcess/app.js
@@ -1,4 +1,12 @@
 function solve() {
-    //TODO
+    const firstNameField = document.getElementById('fname');
+    const lastNameField = document.getElementById('lname');
+    const emailField = document.getElementById('email');
+    const birthField = document.getElementById('birth');
+    const positionField = document.getElementById('position');
+    const salaryField = document.getElementById('salary');
+    const hireWorkerBtn = document.getElementById('add-worker');
+    
 }
-solve()
+
+solve();
