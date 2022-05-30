ea863471f5f4f86b8185c765a3588ebe43050ae7
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-30 08:34:14 +0300

    Add dom fields for manipulation

--- ExamPreparation/Exam13March2022/T01MailsDelivery/app.js
+++ ExamPreparation/Exam13March2022/T01MailsDelivery/app.js
@@ -1,5 +1,13 @@
 function solve() {
-    
+    const recipentField = document.getElementById('recipientName');
+    const titleField = document.getElementById('title');
+    const messageField = document.getElementById('message');
+    const addBtn = document.getElementById('add');
+    const resetBtn = document.getElementById('reset');
+
+    const listOfMails = document.getElementById('list');
+    const sendMails = document.querySelector('.sent-list');
+    const deletedMails = document.querySelector('.delete-list');
 }
 
 solve();
