cba9aa812b18d02b2aa128e521a8a51e3748f21f
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-31 12:29:26 +0300

    Add ArtGallery addArticle function

--- ExamPreparation/Exam12August2021/T02ArtGallery/artGallery.js
+++ ExamPreparation/Exam12August2021/T02ArtGallery/artGallery.js
@@ -9,4 +9,28 @@ class ArtGallery {
         this.listOfArticles = [];
         this.guests = [];
     }
+
+    addArticle(articleModel, articleName, quantity) {
+        if (!this.possibleArticles[articleModel.toLowerCase()]) {
+            throw new Error('This article model is not included in this gallery!');
+        }
+
+        const existingArticle = this.listOfArticles.find(a => a.articleName == articleName);
+        if (!existingArticle) {
+            let model = articleModel.toLowerCase();
+
+            let article = {
+                model,
+                articleName,
+                quantity
+            }
+
+            this.listOfArticles.push(article);
+
+        } else {
+            existingArticle.quantity += quantity;
+        }
+
+        return `Successfully added article ${articleName} with a new quantity- ${quantity}.`;
+    }
 }
