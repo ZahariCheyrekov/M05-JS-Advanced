312418e3aebef2afdb1bb21a5a865e1a41815ab7
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-31 19:41:48 +0300

    Add task changes

--- ExamPreparation/Exam27June2021/T02Restaurant/restaurant.js
+++ ExamPreparation/Exam27June2021/T02Restaurant/restaurant.js
@@ -1,32 +1,27 @@
 class Restaurant {
-    constructor(budgetMoney) {
-        this.budgetMoney = budgetMoney;
+    constructor(budget) {
+        this.budgetMoney = Number(budget);
         this.menu = {};
         this.stockProducts = {};
         this.history = [];
     }
 
     loadProducts(products) {
-        for (const product of products) {
-            let [productName, productQuantity, productTotalPrice] = product.split(' ');
-            productQuantity = Number(productQuantity);
-            productTotalPrice = Number(productTotalPrice);
+        for (let product of products) {
+            let [name, quantity, totalPrice] = product.split(' ');
 
-            if (productTotalPrice <= this.budgetMoney) {
-                if (this.stockProducts[productName]) {
-                    this.stockProducts[productName.productQuantity] += productQuantity;
-                } else {
-                    const product = {
-                        productName,
-                        productQuantity
-                    };
+            quantity = Number(quantity);
+            totalPrice = Number(totalPrice);
 
-                    this.budgetMoney -= productTotalPrice;
+            if (totalPrice <= this.budgetMoney) {
+                if (!this.stockProducts[name]) {
+                    this.stockProducts[name] = 0;
                 }
-
-                this.history.push(`Successfully loaded ${productQuantity} ${productName}`);
+                this.stockProducts[name] += quantity;
+                this.budgetMoney -= totalPrice;
+                this.history.push(`Successfully loaded ${quantity} ${name}`);
             } else {
-                this.history.push(`There was not enough money to load ${productQuantity} ${productName}`);
+                this.history.push(`There was not enough money to load ${quantity} ${name}`);
             }
         }
 
@@ -34,22 +29,24 @@ class Restaurant {
     }
 
     addToMenu(meal, neededProducts, price) {
+
         if (!this.menu[meal]) {
             this.menu[meal] = {
                 products: {},
                 price: Number(price)
             }
 
-            for (const current of neededProducts) {
-                let [productName, productQuantity] = current.split(' ');
-                productQuantity = Number(productQuantity);
+            for (let product of neededProducts) {
+                let [name, quantity] = product.split(' ');
+                quantity = Number(quantity);
+                this.menu[meal].products[name] = quantity;
+            }
 
-                this.menu[meal].products[productName] = productQuantity;
-
-                const numberOfMeals = Object.keys(this.menu).length;
-                return numberOfMeals == 1
-                    ? `Great idea! Now with the ${meal} we have 1 meal in the menu, other ideas?`
-                    : `Great idea! Now with the ${meal} we have ${numberOfMeals} meals in the menu, other ideas?`;
+            const mealCount = Object.keys(this.menu).length;
+            if (mealCount === 1) {
+                return `Great idea! Now with the ${meal} we have 1 meal in the menu, other ideas?`;
+            } else {
+                return `Great idea! Now with the ${meal} we have ${mealCount} meals in the menu, other ideas?`;
             }
         } else {
             return `The ${meal} is already in the our menu, try something different.`;
@@ -57,15 +54,42 @@ class Restaurant {
     }
 
     showTheMenu() {
-        if (Object.keys(this.menu).length == 0) {
-            return 'Our menu is not ready yet, please come later...';
+        if (Object.keys(this.menu).length === 0) {
+            return 'Our menu is not ready yet, please come later...'
+        } else {
+            let output = [];
+            for (let meal in this.menu) {
+                output.push(`${meal} - $ ${this.menu[meal].price}`)
+            }
+            return output.join('\n')
         }
+    }
 
-        let result = [];
-        for (let meal in this.menu) {
-            result.push(`${meal} - $ ${this.menu[meal].price}`);
+    makeTheOrder(meal) {
+        if (!this.menu[meal]) {
+            return `There is not ${meal} yet in our menu, do you want to order something else?`
+        } else {
+            const neededProducts = this.menu[meal].products;
+            const neededArr = Object.keys(this.menu[meal].products)
+
+            for (let product in neededProducts) {
+                if (!this.stockProducts[product] || this.stockProducts[product] < this.menu[meal].products[product]) {
+                    return `For the time being, we cannot complete your order (${meal}), we are very sorry...`
+                } else {
+                    neededArr.splice(neededArr.indexOf(product, 1))
+                }
+            }
+
+            if (neededArr.length === 0) {
+                for (let product in neededProducts) {
+                    this.stockProducts[product] -= neededProducts[product];
+                }
+
+                this.budgetMoney += this.menu[meal].price;
+                return `Your order (${meal}) will be completed in the next 30 minutes and will cost you ${this.menu[meal].price}.`
+            } else {
+                return `For the time being, we cannot complete your order (${meal}), we are very sorry...`
+            }
         }
-
-        return result.join('\n');
     }
 }
