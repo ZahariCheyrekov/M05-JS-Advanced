65ddaafe1775db50f130abdaa281b13d0e0999ab
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-31 10:24:08 +0300

    Add not empty fields validation

--- ExamPreparation/Exam23October2021/T01MusicSite/app.js
+++ ExamPreparation/Exam23October2021/T01MusicSite/app.js
@@ -6,6 +6,18 @@ function solve() {
     const authorField = document.getElementById('author');
     const dateField = document.getElementById('date');
     const addBtn = document.getElementById('add-btn');
+    addBtn.addEventListener('click', addSong);
 
-    
+    function addSong(ev) {
+        ev.preventDefault();
+
+        const genreContent = genreField.value;
+        const nameContent = nameField.value;
+        const authorContent = authorField.value;
+        const dateContent = dateField.value;
+
+        if (!genreContent || !nameContent || !authorContent || !dateContent) {
+            return;
+        }
+    }
 }
