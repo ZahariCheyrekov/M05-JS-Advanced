d8232b41a182ba6f8c493fa1ab09c8f03cd85f81
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-31 21:13:39 +0300

    Get input fields from html

--- ExamPreparation/Exam20February2021/T01SoftBlog/app.js
+++ ExamPreparation/Exam20February2021/T01SoftBlog/app.js
@@ -1,3 +1,13 @@
-function solve(){
-   //Todo: Write your code here!
-  }
+function solve() {
+   const authorField = document.getElementById('creator');
+   const titleField = document.getElementById('title');
+   const contentField = document.getElementById('content');
+   const createBtn = document.querySelector('.btn.create');
+   createBtn.addEventListener('click', addArticle);
+
+   function addArticle(ev) {
+      ev.preventDefault();
+
+      
+   }
+}
