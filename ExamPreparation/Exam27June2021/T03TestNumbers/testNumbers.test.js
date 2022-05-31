8703198e55ccdc5b7d053e164065932ba313f35a
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-31 20:38:41 +0300

    Add test invalid first value

--- ExamPreparation/Exam27June2021/T03TestNumbers/testNumbers.test.js
+++ ExamPreparation/Exam27June2021/T03TestNumbers/testNumbers.test.js
@@ -1,6 +1,22 @@
-const { assert, expect } = requre('chai');
+const { assert, expect } = require('chai');
 const testNumbers = require('./testNumbers');
 
 describe('TestNumbers Tests', () => {
-    
+    describe('Tests should check sumNumbers function', () => {
+        it('should throw error for invalid first type parameter', () => {
+            expect(testNumbers.sumNumbers('invalid', 10)).to.be.undefined;
+        });
+
+
+                // sumNumbers: function (num1, num2) {
+        //     let sum = 0;
+
+        //     if (typeof (num1) !== 'number' || typeof (num2) !== 'number') {
+        //         return undefined;
+        //     } else {
+        //         sum = (num1 + num2).toFixed(2);
+        //         return sum;
+        //     }
+        // },
+    });
 });
