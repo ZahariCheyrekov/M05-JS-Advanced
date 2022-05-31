bdaccc775c648b41a8ca5ad56bc387e8d9849672
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-31 11:18:07 +0300

    Add calcPriceOfBook function error tests

--- ExamPreparation/Exam23October2021/T03Library/library.test.js
+++ ExamPreparation/Exam23October2021/T03Library/library.test.js
@@ -2,5 +2,19 @@ const { assert, expect } = require('chai');
 const library = require('./library');
 
 describe('Library Tests', () => {
+    describe('Tests should check calcPriceOfBook function', () => {
+        it('should throw error for invalid type of first parameter', () => {
+            expect(() => library.calcPriceOfBook(10, 10)).to.throw(Error);
+        });
+
+        it('should throw error for invalid type of second parameter', () => {
+            expect(() => library.calcPriceOfBook('Steve Jobs', 'invalid')).to.throw(Error);
+        });
+
+        it('should throw error for invalid input type parameters', () => {
+            expect(() => library.calcPriceOfBook(10, 'invalid')).to.throw(Error);
+        });
+
     
+    });
 });
