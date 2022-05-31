3ab25451707b650d556bcd5f1b819c71039896a2
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-31 11:48:46 +0300

    Add new elements to created table row

--- ExamPreparation/Exam12August2021/T01FurnitureStore/app.js
+++ ExamPreparation/Exam12August2021/T01FurnitureStore/app.js
@@ -26,7 +26,49 @@ function solve() {
             return;
         }
 
+        const productTr = document.createElement('tr');
+        productTr.classList.add('info');
+
+        const modelName = document.createElement('td');
+        modelName.textContent = modelField.value;
+
+        const modelPrice = document.createElement('td');
+        modelPrice.textContent = Number(priceField.value).toFixed(2);
+
+        productTr.appendChild(modelName);
+        productTr.appendChild(modelPrice);
+
+        const moreBtn = document.createElement('button');
+        moreBtn.textContent = 'More Info';
+        moreBtn.classList.add('moreBtn');
+
+        const productInfo = document.createElement('tr');
+        productInfo.classList.add('hide');
+        const productYear = document.createElement('td');
+        const productDescription = document.createElement('td');
+
         
+
+        productYear.textContent = `Year: ${yearField.value}`;
+        productDescription.setAttribute('colspan', 3);
+        productDescription.textContent = `Description: ${descriptionField.value}`;
+
+        productInfo.appendChild(productYear);
+        productInfo.appendChild(productDescription);
+
+        const buyBtn = document.createElement('button');
+        buyBtn.textContent = 'Buy it';
+        buyBtn.classList.add('buyBtn');
+
+        
+
+        const buttons = document.createElement('td');
+        buttons.appendChild(moreBtn);
+        buttons.appendChild(buyBtn);
+        productTr.appendChild(buttons);
+
+        elementsTree.appendChild(productTr);
+        elementsTree.appendChild(productInfo);
 
         clearInputFields();
     }
