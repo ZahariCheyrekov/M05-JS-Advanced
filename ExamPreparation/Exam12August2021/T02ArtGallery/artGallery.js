491aaaafb9db7459dc9b311f5bc76281d1729c10
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-31 12:59:34 +0300

    Add buyArticle function

--- ExamPreparation/Exam12August2021/T02ArtGallery/artGallery.js
+++ ExamPreparation/Exam12August2021/T02ArtGallery/artGallery.js
@@ -10,6 +10,30 @@ class ArtGallery {
         this.guests = [];
     }
 
+    addArticle(articleModel, articleName, quantity) {
+        const articleByModel = this.findArticleByModel(articleModel);
+
+        if (!articleByModel) {
+            throw new Error('This article model is not included in this gallery!');
+        }
+
+        const existingArticle = this.findArticleByName(articleName);
+        if (!existingArticle) {
+            let model = articleModel.toLowerCase();
+
+            let article = {
+                model,
+                articleName,
+                quantity
+            }
+            this.listOfArticles.push(article);
+        } else {
+            existingArticle.quantity += quantity;
+        }
+
+        return `Successfully added article ${articleName} with a new quantity- ${quantity}.`;
+    }
+
     inviteGuest(guestName, personality) {
         const existingGuest = this.findGuestByName(guestName);
 
@@ -27,6 +51,34 @@ class ArtGallery {
         return `You have successfully invited ${guestName}!`;
     }
 
+    buyArticle(articleModel, articleName, guestName) {
+        const article = this.findArticleByName(articleName);
+        if (!article || article.model != articleModel) {
+            throw new Error('This article is not found!');
+        }
+
+        if (article.quantity == 0) {
+            return `The ${articleName} is not available.`;
+        }
+
+        const guest = this.findGuestByName(guestName);
+        if (!guest) {
+            return 'This guest is not invited.';
+        }
+
+        const neededPoints = this.possibleArticles[articleModel];
+        if (guest.points >= neededPoints) {
+            guest.points -= neededPoints;
+            article.quantity--;
+            guest.purchaseArticle++;
+
+        } else {
+            return 'You need to more points to purchase the article.';
+        }
+
+        return `${guestName} successfully purchased the article worth ${neededPoints} points.`;
+    }
+
     getPointsPerPersonality(personality) {
         const values = {
             Vip: 500,
@@ -34,30 +86,6 @@ class ArtGallery {
         };
 
         return values[personality] ? values[personality] : 50;
-    }
-
-    addArticle(articleModel, articleName, quantity) {
-        const articleByModel = findArticleByModel(articleModel);
-
-        if (!articleByModel) {
-            throw new Error('This article model is not included in this gallery!');
-        }
-
-        const existingArticle = findArticleByName(articleName);
-        if (!existingArticle) {
-            let model = articleModel.toLowerCase();
-
-            let article = {
-                model,
-                articleName,
-                quantity
-            }
-            this.listOfArticles.push(article);
-        } else {
-            existingArticle.quantity += quantity;
-        }
-
-        return `Successfully added article ${articleName} with a new quantity- ${quantity}.`;
     }
 
     findArticleByName(name) {
