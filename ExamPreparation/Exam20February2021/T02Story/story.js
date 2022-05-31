8f5dd177ffc63bdf27b69d566ea4c1852643948b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-31 22:32:51 +0300

    Add Story class constructor

--- ExamPreparation/Exam20February2021/T02Story/story.js
+++ ExamPreparation/Exam20February2021/T02Story/story.js
@@ -1,3 +1,10 @@
 class Story {
-   
+    constructor(title, creator) {
+        this.title = title;
+        this.creator = creator;
+        this.comments = [];
+        this.likes= [];
+    }
+
+    //TODO: Add implementation to the functions
 }
