1e5283666b5fadb5169c1750e89d2c9fb0e4d409
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-31 11:18:38 +0300

    Add calcPriceOfBook function successful test

--- ExamPreparation/Exam23October2021/T03Library/library.test.js
+++ ExamPreparation/Exam23October2021/T03Library/library.test.js
@@ -15,6 +15,22 @@ describe('Library Tests', () => {
             expect(() => library.calcPriceOfBook(10, 'invalid')).to.throw(Error);
         });
 
-    
+        it('should return that price for the book of year 2011 is 20', () => {
+            const expectedMessage = 'Price of Steve Jobs is 20.00';
+            const actualMessage = library.calcPriceOfBook('Steve Jobs', 2011);
+            assert.equal(expectedMessage, actualMessage);
+        });
+
+        it('should return the price for the book of year 1800', () => {
+            const expectedMessage = 'Price of IDK is 10.00';
+            const actualMessage = library.calcPriceOfBook('IDK', 1800);
+            assert.equal(expectedMessage, actualMessage);
+        });
+
+        it('should return the price for the book of year 1980', () => {
+            const expectedMessage = 'Price of Heroes is 10.00';
+            const actualMessage = library.calcPriceOfBook('Heroes', 1980);
+            assert.equal(expectedMessage, actualMessage);
+        });
     });
 });
