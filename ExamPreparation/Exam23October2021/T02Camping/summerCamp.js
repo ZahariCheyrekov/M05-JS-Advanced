7d95f1868087023b001bdf6af211db62ac31573b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-31 11:06:20 +0300

    Add SummerCamp input data for testing

--- ExamPreparation/Exam23October2021/T02Camping/summerCamp.js
+++ ExamPreparation/Exam23October2021/T02Camping/summerCamp.js
@@ -94,3 +94,13 @@ class SummerCamp {
         return result.join('\n');
     }
 }
+
+const summerCamp = new SummerCamp("Jane Austen", "Pancharevo Sofia 1137, Bulgaria");
+console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
+console.log(summerCamp.timeToPlay("Battleship", "Petar Petarson"));
+console.log(summerCamp.registerParticipant("Sara Dickinson", "child", 200));
+console.log(summerCamp.timeToPlay("WaterBalloonFights", "Petar Petarson", "Sara Dickinson"));
+console.log(summerCamp.registerParticipant("Dimitur Kostov", "student", 300));
+console.log(summerCamp.timeToPlay("WaterBalloonFights", "Petar Petarson", "Dimitur Kostov"));
+
+console.log(summerCamp.toString());
