d732aeb93056ac939a23b4dc3b674679b5ef5522
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-31 18:10:04 +0300

    Get field input values

--- ExamPreparation/Exam27June2021/T01TravelAgency/solution.js
+++ ExamPreparation/Exam27June2021/T01TravelAgency/solution.js
@@ -7,8 +7,35 @@ function solution() {
   const adressField = document.getElementById('address');
   const codeField = document.getElementById('code');
   const submitBtn = document.getElementById('submitBTN');
+  submitBtn.addEventListener('click', submitReservation);
 
   const ulList = document.getElementById('infoPreview');
   const editBtn = document.getElementById('editBTN');
   const continueBtn = document.getElementById('continueBTN');
+
+  function submitReservation() {
+    const nameContent = fullNameField.value;
+    const emailContent = emailField.value;
+    const phoneContent = numberField.value;
+    const addressContent = adressField.value;
+    const codeContent = codeField.value;
+
+    if (!nameContent || !emailContent) {
+      return;
+    }
+
+    submitBtn.disabled = true;
+    editBtn.disabled = false;
+    continueBtn.disabled = false;
+
+    clearInputFields();
+  }
+
+  function clearInputFields() {
+    fullNameField.value = '';
+    emailField.value = '';
+    numberField.value = '';
+    adressField.value = '';
+    codeField.value = '';
+  }
 }
