01c6a75eb9ed490cbf365cd79e4b1c4ebad00ef1
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-31 10:21:26 +0300

    Get fields from html

--- ExamPreparation/Exam23October2021/T01MusicSite/app.js
+++ ExamPreparation/Exam23October2021/T01MusicSite/app.js
@@ -1,5 +1,11 @@
 window.addEventListener('load', solve);
 
 function solve() {
-    //TODO
+    const genreField = document.getElementById('genre');
+    const nameField = document.getElementById('name');
+    const authorField = document.getElementById('author');
+    const dateField = document.getElementById('date');
+    const addBtn = document.getElementById('add-btn');
+
+    
 }
