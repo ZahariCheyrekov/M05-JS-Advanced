db46c9a9bdce96acab8859093abe331a6dbb93df
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-31 14:24:20 +0300

    Add test for invalid projection type

--- ExamPreparation/Exam12August2021/T03Cinema/cinema.test.js
+++ ExamPreparation/Exam12August2021/T03Cinema/cinema.test.js
@@ -2,16 +2,7 @@ const { assert, expect } = require('chai');
 const cinema = require('./cinema');
 
 describe('Cinema Tests', () => {
-    describe('Tests should check showMovie function', () => {
-        // showMovies: function (movieArr) {
-        //     if (movieArr.length == 0) {
-        //         return 'There are currently no movies to show.';
-        //     } else {
-        //         let result = movieArr.join(', ');
-        //         return result;
-        //     }
-        // },
-
+    describe('Tests should check showMovies fucntion', () => {
         it('should return that they are currently no movies to show', () => {
             const expectedMessage = 'There are currently no movies to show.';
             const actualMessage = cinema.showMovies([]);
@@ -23,5 +14,12 @@ describe('Cinema Tests', () => {
             const actualMessage = cinema.showMovies(['partOne', 'partTwo']);
             assert.equal(expectedMessage, actualMessage);
         });
+    });
+
+    describe('Tests should check ticketPrice function', () => {
+        it('should throw error for invalid projection type', () => {
+            expect(() => cinema.ticketPrice('invalid')).to.throw(Error);
+        });
+
     });
 });
