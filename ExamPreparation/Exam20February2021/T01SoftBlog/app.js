426fe0a1c159b2b17ede6e0c8095beb33e44f229
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-31 21:15:25 +0300

    Add clearInputFields function

--- ExamPreparation/Exam20February2021/T01SoftBlog/app.js
+++ ExamPreparation/Exam20February2021/T01SoftBlog/app.js
@@ -1,6 +1,7 @@
 function solve() {
    const authorField = document.getElementById('creator');
    const titleField = document.getElementById('title');
+   const categoryField = document.getElementById('category');
    const contentField = document.getElementById('content');
    const createBtn = document.querySelector('.btn.create');
    createBtn.addEventListener('click', addArticle);
@@ -8,6 +9,13 @@ function solve() {
    function addArticle(ev) {
       ev.preventDefault();
 
-      
+      clearInputFields();
+   }
+
+   function clearInputFields() {
+      authorField.value = '';
+      titleField.value = '';
+      categoryField.value = '';
+      contentField.value = '';
    }
 }
