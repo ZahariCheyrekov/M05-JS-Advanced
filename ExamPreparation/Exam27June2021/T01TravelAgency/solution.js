1339b8348f6817b44b333f36bcc5d99395b6ea5e
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-31 18:03:35 +0300

    Get buttons from html

--- ExamPreparation/Exam27June2021/T01TravelAgency/solution.js
+++ ExamPreparation/Exam27June2021/T01TravelAgency/solution.js
@@ -6,4 +6,9 @@ function solution() {
   const numberField = document.getElementById('phone');
   const adressField = document.getElementById('address');
   const codeField = document.getElementById('code');
+  const submitBtn = document.getElementById('submitBTN');
+
+  const ulList = document.getElementById('infoPreview');
+  const editBtn = document.getElementById('editBTN');
+  const continueBtn = document.getElementById('continueBTN');
 }
