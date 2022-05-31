9786dd345c6a8d480e345ff60ee809f2b563bd08
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-31 11:46:46 +0300

    Get input fields from html

--- ExamPreparation/Exam12August2021/T01FurnitureStore/app.js
+++ ExamPreparation/Exam12August2021/T01FurnitureStore/app.js
@@ -1,5 +1,14 @@
 window.addEventListener('load', solve);
 
 function solve() {
-    //TO DO
+    const modelField = document.getElementById('model');
+    const yearField = document.getElementById('year');
+    const descriptionField = document.getElementById('description');
+    const priceField = document.getElementById('price');
+    const addButton = document.getElementById('add');
+    const elementsTree = document.getElementById('furniture-list');
+    const totalPriceElement = document.querySelector('.total-price');
+
+    addButton.addEventListener('click', onClick);
+
 }
