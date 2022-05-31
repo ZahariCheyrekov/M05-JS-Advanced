471a8671957f9b84bfe1fba03ac077f47a88c040
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-31 21:22:30 +0300

    Create article and the article elements

--- ExamPreparation/Exam20February2021/T01SoftBlog/app.js
+++ ExamPreparation/Exam20February2021/T01SoftBlog/app.js
@@ -6,8 +6,40 @@ function solve() {
    const createBtn = document.querySelector('.btn.create');
    createBtn.addEventListener('click', addArticle);
 
+   const articlesSection = document.querySelector('.site-content main section');
+
    function addArticle(ev) {
       ev.preventDefault();
+
+      const article = document.createElement('article');
+      const title = document.createElement('h1');
+      title.textContent = titleField.value;
+
+      const category = document.createElement('p');
+      category.textContent = 'Category:';
+
+      const categoryType = document.createElement('strong');
+      categoryType.textContent = categoryField.value;
+
+      const creator = document.createElement('p');
+      creator.textContent = 'Creator:';
+
+      const creatorName = document.createElement('strong');
+      creatorName.textContent = authorField.value;
+
+      const content = document.createElement('p');
+      content.textContent = contentField.value;
+
+      const divBtns = document.createElement('div');
+      divBtns.classList.add('buttons');
+
+      const deleteBtn = document.createElement('button');
+      deleteBtn.classList.add('btn delete');
+      deleteBtn.textContent = 'Delete';
+
+      const archiveBtn = document.createElement('button');
+      archiveBtn.classList.add('btn archive');
+      archiveBtn.textContent = 'Archive';
 
       clearInputFields();
    }
