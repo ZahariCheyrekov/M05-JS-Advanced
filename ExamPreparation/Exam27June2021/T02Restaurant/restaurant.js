df347e0d164a79efadbda3f95eeb9e485303d0df
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-05-31 19:14:51 +0300

    Add load products function

--- ExamPreparation/Exam27June2021/T02Restaurant/restaurant.js
+++ ExamPreparation/Exam27June2021/T02Restaurant/restaurant.js
@@ -5,4 +5,31 @@ class Restaurant {
         this.stockProducts = {};
         this.history = [];
     }
+
+    loadProducts(products) {
+        for (const product of products) {
+            let [productName, productQuantity, productTotalPrice] = product.split(' ');
+            productQuantity = Number(productQuantity);
+            productTotalPrice = Number(productTotalPrice);
+
+            if (productTotalPrice <= this.budgetMoney) {
+                if (this.stockProducts[productName]) {
+                    this.stockProducts[productName.productQuantity] += productQuantity;
+                } else {
+                    const product = {
+                        productName,
+                        productQuantity
+                    };
+
+                    this.budgetMoney -= productTotalPrice;
+                }
+
+                this.history.push(`Successfully loaded ${productQuantity} ${productName}`);
+            } else {
+                this.history.push(`There was not enough money to load ${productQuantity} ${productName}`);
+            }
+        }
+
+        return this.history.join('\n');
+    }
 }
