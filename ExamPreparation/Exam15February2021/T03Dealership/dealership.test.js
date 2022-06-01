66cf4396ba60a47f352e6d78829b9c14c5e33ac4
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-01 13:25:17 +0300

    Add test for low euroCategory

--- ExamPreparation/Exam15February2021/T03Dealership/dealership.test.js
+++ ExamPreparation/Exam15February2021/T03Dealership/dealership.test.js
@@ -1,4 +1,4 @@
-const { assert, expect } = require('chai');
+const { expect } = require('chai');
 const dealership = require('./dealership');
 
 describe('Dealership Tests', () => {
@@ -18,6 +18,23 @@ describe('Dealership Tests', () => {
     describe('Tests should check carEquipment function', () => {
         it('should return the selected cars in array', () => {
             expect(dealership.carEquipment(['new', 'old'], [0, 1])).to.deep.equal(['new', 'old']);
+        });
+    });
+
+    describe('Tests should check euroCategory', () => {
+        // euroCategory: function (category) {
+        //     if (category >= 4) {
+        //         let price = this.newCarCost('Audi A4 B8', 30000);
+        //         let total = price - (price * 0.05);
+
+        //         return `We have added 5% discount to the final price: ${total}.`;
+
+        //     } else {
+        //         return 'Your euro category is low, so there is no discount from the final price!';
+        //     }
+        // }
+        it('should return that the euro category is low and there is no discount for it', () => {
+            expect(dealership.euroCategory(3)).to.equal('Your euro category is low, so there is no discount from the final price!');
         });
     });
 });
