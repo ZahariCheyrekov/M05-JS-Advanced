d6de455259a7ae26f7742ba9f2f5339ece17611c
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-01 22:11:39 +0300

    Add beforeEach function

--- ExamPreparation/Exam10December2019/T02ChristmasMovies/christmasMovies.test.js
+++ ExamPreparation/Exam10December2019/T02ChristmasMovies/christmasMovies.test.js
@@ -2,7 +2,8 @@ const { assert, expect } = require('chai');
 const ChristmasMovies = require('./christmasMovies');
 
 describe('ChristmasMovies Tests', () => {
+    let instance = {};
+    beforeEach(() => instance = new ChristmasMovies());
+
     
-
-
 });
