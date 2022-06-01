5e4a95c56c18e99d0746e99d82123c59bb2e34f1
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-01 12:18:58 +0300

    Add eventListener to addGiftBtn

--- ExamPreparation/Exam15February2021/T01ChristmasGiftsDelivery/app.js
+++ ExamPreparation/Exam15February2021/T01ChristmasGiftsDelivery/app.js
@@ -1,4 +1,5 @@
 function solution() {
     const inputField = document.querySelector('.card div input');
     const addGiftBtn = document.querySelector('.card div button');
+    addGiftBtn.addEventListener('click', addGift);
 }
