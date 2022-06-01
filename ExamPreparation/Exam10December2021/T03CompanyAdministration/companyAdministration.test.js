1d06d430a9de0355daa324d6f316a1e4517e3a9b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-01 22:58:25 +0300

    Delete old test cases

--- ExamPreparation/Exam10December2021/T03CompanyAdministration/companyAdministration.test.js
+++ ExamPreparation/Exam10December2021/T03CompanyAdministration/companyAdministration.test.js
@@ -2,19 +2,5 @@ const { assert, expect } = require('chai');
 const companyAdministration = require('./companyAdministration');
 
 describe('CompanyAdministration Tests', () => {
-    describe('Tests should check hiringEmployee function', () => {
-      
-    });
-
-    describe('Tests should check calculateSalary function', () => {
-       
-
-       
-    });
-
-    describe('Tests should check firedEmployee function', () => {
-   
-
-
-    });
+    
 });
