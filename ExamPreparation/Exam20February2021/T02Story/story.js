1979747bf65740d329b0be1ddf2d424e2c0e212c
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-01 10:54:03 +0300

    Add like function

--- ExamPreparation/Exam20February2021/T02Story/story.js
+++ ExamPreparation/Exam20February2021/T02Story/story.js
@@ -13,6 +13,21 @@ class Story {
             return `${this.likes[0]} likes this story!`
         }
 
-        return `${this.likes[0]} and ${this.likes.length-1} others like this story!`;
+        return `${this.likes[0]} and ${this.likes.length - 1} others like this story!`;
     }
+
+    like(username) {
+        if (this.likes.includes(username)) {
+            throw new Error('You can\'t like the same story twice!');
+        }
+
+        if (username == this.creator) {
+            throw new Error('You can\'t like your own story!');
+        }
+
+        this.like.push(username);
+        return `${username} liked ${this.title}!`;
+    }
+
+    
 }
