ac4eb9ac0017f5745c8c8ff5c17f697328da8bef
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-01 23:11:37 +0300

    Add test invalid hours string

--- ExamPreparation/Exam10December2021/T03CompanyAdministration/companyAdministration.test.js
+++ ExamPreparation/Exam10December2021/T03CompanyAdministration/companyAdministration.test.js
@@ -3,16 +3,6 @@ const companyAdministration = require('./companyAdministration');
 
 describe('CompanyAdministration Tests', () => {
     describe('Tests dhould check hiringEmployee function', () => {
-        // hiringEmployee(name, position, yearsExperience) {
-        //     if (position == "Programmer") {
-        //         if (yearsExperience >= 3) {
-        //             return `${name} was successfully hired for the position ${position}.`;
-        //         } else {
-        //             return `${name} is not approved for this position.`;
-        //         }
-        //     }
-        //     throw new Error(`We are not looking for workers for this position.`);
-        // },
         it('should throw error for invalid position Dog', () => {
             expect(() => companyAdministration.hiringEmployee('Doggy', 'Dog', 10)).to.throw('We are not looking for workers for this position.');
         });
@@ -39,6 +29,23 @@ describe('CompanyAdministration Tests', () => {
         it('should return that employee was not approved for the position with 0 years of experience', () => {
             expect(companyAdministration.hiringEmployee('Prog1234', 'Programmer', 0))
                 .to.equal('Prog1234 is not approved for this position.');
+        });
+    });
+
+    describe('Tests should check calculateSalary function', () => {
+        // calculateSalary(hours) {
+        //     let payPerHour = 15;
+        //     let totalAmount = payPerHour * hours;
+
+        //     if (typeof hours !== "number" || hours < 0) {
+        //         throw new Error("Invalid hours");
+        //     } else if (hours > 160) {
+        //         totalAmount += 1000;
+        //     }
+        //     return totalAmount;
+        // },
+        it('should throw error for invalid hours of type string', () => {
+            expect(() => companyAdministration.calculateSalary('salary', 3)).to.throw('Invalid hours');
         });
     });
 });
