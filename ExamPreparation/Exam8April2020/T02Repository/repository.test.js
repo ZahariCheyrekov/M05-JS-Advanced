b46d0129604ae5bbbe4e2bc10945a31145d2ef75
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-01 17:44:54 +0300

    Add test for Map size

--- ExamPreparation/Exam8April2020/T02Repository/repository.test.js
+++ ExamPreparation/Exam8April2020/T02Repository/repository.test.js
@@ -1,15 +1,20 @@
 const { expect } = require('chai');
-const { before } = require('mocha');
 const { Repository } = require('./repository');
 
 describe("Repository Tests", () => {
     let instance = {};
 
-    beforeEach(() => {
-        beforeEach(() => instance = new Repository({
-            name: 'string',
-            age: 'number',
-            birthday: 'object'
-        }))
+    beforeEach(() => instance = new Repository({
+        name: 'string',
+        age: 'number',
+        birthday: 'object'
+    }));
+
+    describe('Test should check number of Map elements', () => {
+        it('should return that Map size is 0', () => {
+            expect(instance.count).to.equal(0);
+        });
     });
+
+    
 });
