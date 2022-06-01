2a5b0f4483a6898219a25a40b7f66dc9ca67043e
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-01 17:16:32 +0300

    Add repo declaration

--- ExamPreparation/Exam8April2020/T02Repository/repository.test.js
+++ ExamPreparation/Exam8April2020/T02Repository/repository.test.js
@@ -1,6 +1,12 @@
 const { expect } = require('chai');
+const { before } = require('mocha');
 const { Repository } = require('./repository');
 
 describe("Repository Tests", () => {
+    let repo;
 
+    beforeEach(() => {
+        let properties = {
+        };
+    });
 });
