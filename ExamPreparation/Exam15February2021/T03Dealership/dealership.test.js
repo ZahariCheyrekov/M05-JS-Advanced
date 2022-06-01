8c46a1b9f49b470038052fd259fa4b3d0252e65f
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-01 13:19:50 +0300

    Add test for new car cost

--- ExamPreparation/Exam15February2021/T03Dealership/dealership.test.js
+++ ExamPreparation/Exam15February2021/T03Dealership/dealership.test.js
@@ -2,30 +2,16 @@ const { assert, expect } = require('chai');
 const dealership = require('./dealership');
 
 describe('Dealership Tests', () => {
-    // newCarCost: function (oldCarModel, newCarPrice) {
-    //     let discountForOldCar = {
-    //         'Audi A4 B8': 15000,
-    //         'Audi A6 4K': 20000,
-    //         'Audi A8 D5': 25000,
-    //         'Audi TT 8J': 14000,
-    //     }
-
-    //     if (discountForOldCar.hasOwnProperty(oldCarModel)) {
-    //         let discount = discountForOldCar[oldCarModel];
-    //         let finalPrice = newCarPrice - discount;
-
-    //         return finalPrice;
-
-    //     } else {
-    //         return newCarPrice;
-    //     }
-    // },
     describe('Tests should check neCarCost function', () => {
         it('should return the final price for a old car', () => {
             expect(dealership.newCarCost('Audi A4 B8', 15_500)).to.equal(500);
             expect(dealership.newCarCost('Audi A6 4K', 20_500)).to.equal(500);
             expect(dealership.newCarCost('Audi A8 D5', 25_500)).to.equal(500);
             expect(dealership.newCarCost('Audi TT 8J', 14_500)).to.equal(500);
+        });
+
+        it('should return the new car price for a new car', () => {
+            expect(dealership.newCarCost('new', 3_500_00)).to.equal(3_500_00);
         });
     });
 });
