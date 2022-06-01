180fc6ed7a25a27d639a0b8685e5bf0339a1464a
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-01 23:43:41 +0300

    Add companyAdministration tests

--- ExamPreparation/Exam10December2021/T03CompanyAdministration/companyAdministration.test.js
+++ ExamPreparation/Exam10December2021/T03CompanyAdministration/companyAdministration.test.js
@@ -33,25 +33,85 @@ describe('CompanyAdministration Tests', () => {
     });
 
     describe('Tests should check calculateSalary function', () => {
-        // calculateSalary(hours) {
-        //     let payPerHour = 15;
-        //     let totalAmount = payPerHour * hours;
-
-        //     if (typeof hours !== "number" || hours < 0) {
-        //         throw new Error("Invalid hours");
-        //     } else if (hours > 160) {
-        //         totalAmount += 1000;
-        //     }
-        //     return totalAmount;
-        // },
         it('should throw error for invalid hours of type string', () => {
-            expect(() => companyAdministration.calculateSalary('salary', 3)).to.throw('Invalid hours');
+            expect(() => companyAdministration.calculateSalary('salary')).to.throw('Invalid hours');
         });
 
         it('should throw error for invalid hours of type null', () => {
-            expect(() => companyAdministration.calculateSalary(null, 3)).to.throw('Invalid hours');
+            expect(() => companyAdministration.calculateSalary(null)).to.throw('Invalid hours');
         });
 
+        it('should throw error for invalid hours less than zero', () => {
+            expect(() => companyAdministration.calculateSalary(-3)).to.throw('Invalid hours');
+        });
 
+        it('should throw error for invalid hours undefined', () => {
+            expect(() => companyAdministration.calculateSalary(undefined)).to.throw('Invalid hours');
+        });
+
+        it('should return the totalAmount for zero hours', () => {
+            expect(companyAdministration.calculateSalary(0)).to.equal(0);
+        });
+
+        it('should return the totalAmount for 160 hours', () => {
+            expect(companyAdministration.calculateSalary(160)).to.equal(2400);
+        });
+
+        it('should return the totalAmount for 161 hours', () => {
+            expect(companyAdministration.calculateSalary(161)).to.equal(3415);
+        });
+    });
+
+    describe('Tests should check firedEmployee function', () => {
+        it('should throw error for invalid index value', () => {
+            expect(() => companyAdministration.firedEmployee(['Sam', 'Mark', 'Emma'], 'really'))
+                .to.throw('Invalid input');
+        });
+
+        it('should throw error for array value not array', () => {
+            expect(() => companyAdministration.firedEmployee(10, 10)).to.throw('Invalid input');
+            expect(() => companyAdministration.firedEmployee()).to.throw('Invalid input');
+            expect(() => companyAdministration.firedEmployee(NaN, 'str')).to.throw('Invalid input');
+            expect(() => companyAdministration.firedEmployee(NaN)).to.throw('Invalid input');
+        });
+
+        it('should throw error for index not integer value', () => {
+            expect(() => companyAdministration.firedEmployee(['Sam', 'Mark', 'Emma'], 5.42324))
+                .to.throw('Invalid input');
+            expect(() => companyAdministration.firedEmployee(['Sam', 'Mark', 'Emma'], 0.42324))
+                .to.throw('Invalid input');
+            expect(() => companyAdministration.firedEmployee(['Sam', 'Mark', 'Emma'], -5.42324))
+                .to.throw('Invalid input');
+        });
+
+        it('should throw error for invalid index value less than zero', () => {
+            expect(() => companyAdministration.firedEmployee(['Sam', 'Mark', 'Emma'], -10))
+                .to.throw('Invalid input');
+        });
+
+        it('should throw error for invalid index value equal to array length', () => {
+            expect(() => companyAdministration.firedEmployee(['Sam', 'Mark', 'Emma'], 3))
+                .to.throw('Invalid input');
+        });
+
+        it('should throw error for invalid index value bigger than array length', () => {
+            expect(() => companyAdministration.firedEmployee(['Sam', 'Mark', 'Emma'], 9))
+                .to.throw('Invalid input');
+        });
+
+        it('should remove employee at the given index 0', () => {
+            expect(companyAdministration.firedEmployee(['Sam', 'Mark', 'Emma'], 0))
+                .to.equal('Mark, Emma');
+        });
+
+        it('should remove employee at the given index 1', () => {
+            expect(companyAdministration.firedEmployee(['Sam', 'Mark', 'Emma'], 1))
+                .to.deep.equal('Sam, Emma');
+        });
+
+        it('should remove employee at the given index 2', () => {
+            expect(companyAdministration.firedEmployee(['Sam', 'Mark', 'Emma'], 2))
+                .to.equal('Sam, Mark');
+        });
     });
 });
