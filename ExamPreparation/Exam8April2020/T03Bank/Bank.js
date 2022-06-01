be8e255a62b5caecf7f3a74462085bd8105e3d9b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-01 18:17:39 +0300

    Add newCustomer function

--- ExamPreparation/Exam8April2020/T03Bank/Bank.js
+++ ExamPreparation/Exam8April2020/T03Bank/Bank.js
@@ -4,7 +4,11 @@ class Bank {
         this.allCustomers = [];
     }
 
-   
+    newCustomer(customer) {
+        if (this.allCustomers.find(c => c.persnoalId === customer.personalId)) {
+            throw new Error(`${customer.firstName} ${customer.lastName} is already our customer!`);
+        }
+        this.allCustomers.push(customer);
+        return customer;
+    }
 }
-
-
