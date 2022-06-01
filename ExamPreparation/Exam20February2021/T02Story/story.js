a6b362cfe4f98671230726c765f7caee8b907ab7
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-01 11:23:29 +0300

    Add toString function

--- ExamPreparation/Exam20February2021/T02Story/story.js
+++ ExamPreparation/Exam20February2021/T02Story/story.js
@@ -2,66 +2,114 @@ class Story {
     constructor(title, creator) {
         this.title = title;
         this.creator = creator;
-        this.comments = [];
-        this.likes = [];
+        this._comments = [];
+        this._likes = [];
     }
 
     get likes() {
-        if (this.likes.length == 0) {
+        if (this._likes.length === 0) {
             return `${this.title} has 0 likes`;
-        } else if (this.likes == 1) {
-            return `${this.likes[0]} likes this story!`
+        } else if (this._likes.length === 1) {
+            return `${this._likes[0]} likes this story!`;
         }
 
-        return `${this.likes[0]} and ${this.likes.length - 1} others like this story!`;
+        return `${this._likes[0]} and ${this._likes.slice(1).length} others like this story!`;
     }
 
     like(username) {
-        if (this.likes.includes(username)) {
-            throw new Error('You can\'t like the same story twice!');
+        if (this._likes.includes(username)) {
+            throw new Error("You can't like the same story twice!");
         }
 
-        if (username == this.creator) {
-            throw new Error('You can\'t like your own story!');
+        if (username === this.creator) {
+            throw new Error("You can't like your own story!");
         }
 
-        this.like.push(username);
+        this._likes.push(username);
         return `${username} liked ${this.title}!`;
     }
 
     dislike(username) {
-        if (!this.likes.includes(username)) {
-            throw new Error('You can\'t dislike this story!');
+        if (!this._likes.includes(username)) {
+            throw new Error("You can't dislike this story!");
         }
 
-        this.likes.filter(user => user !== username);
-        return `${this.username} disliked ${this.title}`;
+        this._likes = this._likes.filter(user => user !== username);
+
+        return `${username} disliked ${this.title}`;
     }
 
     comment(username, content, id) {
-        const comment = {
-            id,
-            username,
-            content,
-            replies: [],
+        let comment = {
+            Id: id,
+            Username: username,
+            Content: content,
+            Replies: [],
         };
 
-        const commentWithId = this.comments.find(c => c.id == id);
+        let commentWithId = this._comments.find(c => c.Id === id);
 
         if (commentWithId) {
-            const replyID = Number(commentWithId.id + '.' + (commentWithId.replies.length + 1));
-            const reply = {
-                idd: replyID,
-                username,
-                content,
+            let replyID = Number(commentWithId.Id + '.' + (commentWithId.Replies.length + 1));
+            let reply = {
+                Id: replyID,
+                Username: username,
+                Content: content,
             };
 
-            commentWithId.replies.push(reply);
+            commentWithId.Replies.push(reply);
+
             return 'You replied successfully';
         }
 
-        comment.id = this.comments.length + 1;
-        this.comments.push(comment);
+        comment.Id = this._comments.length + 1;
+        this._comments.push(comment);
+
         return `${username} commented on ${this.title}`;
     }
+
+    toString(sortingType) {
+        let result = [];
+        result.push(`Title: ${this.title}`);
+        result.push(`Creator: ${this.creator}`);
+        result.push(`Likes: ${this._likes.length}`);
+        result.push('Comments:');
+
+        if (this._comments.length > 0) {
+            let sortedComments = this.sortCriteria(this._comments, sortingType);
+            result.push(this.sortThem(sortedComments, sortingType));
+        }
+        return result.join('\n');
+    }
+
+    sortCriteria(commentsOrReplies, criteria) {
+        let copyCommentsOrReplies = commentsOrReplies.slice();
+        let sortedCommentsOrReplies = null;
+
+        if (criteria === 'asc') {
+            sortedCommentsOrReplies = copyCommentsOrReplies.sort((a, b) => a.Id - b.Id);
+        } else if (criteria === 'desc') {
+            sortedCommentsOrReplies = copyCommentsOrReplies.sort((a, b) => b.Id - a.Id);
+        } else if (criteria === 'username') {
+            sortedCommentsOrReplies = copyCommentsOrReplies.sort((a, b) => a.Username.localeCompare(b.Username));
+        }
+
+        return sortedCommentsOrReplies;
+    }
+
+    sortThem(sortedComments, criteria) {
+        let commentAndReplies = [];
+
+        for (let comment of sortedComments) {
+            commentAndReplies.push(`-- ${comment.Id}. ${comment.Username}: ${comment.Content}`);
+
+            if (comment.Replies.length > 0) {
+                let sortedReplies = this.sortCriteria(comment.Replies, criteria);
+                sortedReplies.forEach(r => commentAndReplies.push(`--- ${r.Id}. ${r.Username}: ${r.Content}`));
+            }
+        }
+
+        return commentAndReplies.join('\n');
+    }
 }
+
