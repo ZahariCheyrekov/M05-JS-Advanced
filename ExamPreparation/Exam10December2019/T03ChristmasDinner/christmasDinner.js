165c6879eb8288e14c1d4264d1b187724ed1ae39
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-01 22:50:36 +0300

    Add class constructor

--- ExamPreparation/Exam10December2019/T03ChristmasDinner/christmasDinner.js
+++ ExamPreparation/Exam10December2019/T03ChristmasDinner/christmasDinner.js
@@ -1,3 +1,9 @@
 class ChristmasDinner {
-    
+    constructor(budget) {
+        this._budget = budget;
+        this.dishes = [];
+        this.products = [];
+        this.guests = {};
+    }
+
 }
