c94049965de1bde6cf2ece8970c3b0acbab81469
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-01 12:20:10 +0300

    Add clearInputField function

--- ExamPreparation/Exam15February2021/T01ChristmasGiftsDelivery/app.js
+++ ExamPreparation/Exam15February2021/T01ChristmasGiftsDelivery/app.js
@@ -2,4 +2,13 @@ function solution() {
     const inputField = document.querySelector('.card div input');
     const addGiftBtn = document.querySelector('.card div button');
     addGiftBtn.addEventListener('click', addGift);
+
+    function addGift() {
+
+        clearInputField();
+    }
+
+    function clearInputField() {
+        inputField.value = '';
+    }
 }
