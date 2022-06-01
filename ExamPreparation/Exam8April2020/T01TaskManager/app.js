db2a19abe772b8980d1d37927f987971e054dd56
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-01 16:02:15 +0300

    Add data validation logic

--- ExamPreparation/Exam8April2020/T01TaskManager/app.js
+++ ExamPreparation/Exam8April2020/T01TaskManager/app.js
@@ -3,4 +3,20 @@ function solve() {
     const descriptioField = document.createElement('description');
     const dateField = document.getElementById('date');
     const addBtn = document.getElementById('add');
+
+    addBtn.addEventListener('click', addTask);
+
+    function addTask(ev) {
+        ev.preventDefault();
+
+        const task = taskField.value;
+        const description = descriptioField.value;
+        const date = dateField.value;
+
+        if (!task || !description || !date) {
+            return;
+        }
+
+        
+    }
 }
