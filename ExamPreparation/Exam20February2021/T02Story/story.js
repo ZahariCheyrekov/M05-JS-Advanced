299a261c3af8738fc0c9373242b9c19507c602e8
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-01 10:51:16 +0300

    Add get likes function

--- ExamPreparation/Exam20February2021/T02Story/story.js
+++ ExamPreparation/Exam20February2021/T02Story/story.js
@@ -3,8 +3,16 @@ class Story {
         this.title = title;
         this.creator = creator;
         this.comments = [];
-        this.likes= [];
+        this.likes = [];
     }
 
-    //TODO: Add implementation to the functions
+    get likes() {
+        if (this.likes.length == 0) {
+            return `${this.title} has 0 likes`;
+        } else if (this.likes == 1) {
+            return `${this.likes[0]} likes this story!`
+        }
+
+        return `${this.likes[0]} and ${this.likes.length-1} others like this story!`;
+    }
 }
