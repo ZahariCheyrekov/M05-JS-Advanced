b953037c22f20d7dabd82405ea66e1021b9c92e9
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-01 22:13:48 +0300

    Add tests for the constructor

--- ExamPreparation/Exam10December2019/T02ChristmasMovies/christmasMovies.test.js
+++ ExamPreparation/Exam10December2019/T02ChristmasMovies/christmasMovies.test.js
@@ -5,5 +5,11 @@ describe('ChristmasMovies Tests', () => {
     let instance = {};
     beforeEach(() => instance = new ChristmasMovies());
 
-    
+    describe('Test should check the constructor functionality', () => {
+        it('should return that the instance properties are not undefined', () => {
+            expect(instance.movieCollection).to.not.be.undefined;
+            expect(instance.watched).to.not.be.undefined;
+            expect(instance.actors).to.not.be.undefined;
+        });
+    });
 });
