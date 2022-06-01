8638358cb676d0e5d9b8ab1d985c72c7a5e99026
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-01 18:22:18 +0300

    Add depositMoney function

--- ExamPreparation/Exam8April2020/T03Bank/Bank.js
+++ ExamPreparation/Exam8April2020/T03Bank/Bank.js
@@ -12,9 +12,42 @@ class Bank {
         return customer;
     }
 
-  
+    depositMoney(personalId, amount) {
+        const customer = this.findCustomerById(personalId);
+
+        if (!customer) {
+            throw new Error('We have no customer with this ID!')
+        }
+
+        if (!customer.hasOwnProperty('totalMoney')) {
+            customer.totalMoney = 0;
+        }
+
+        if (!customer.hasOwnProperty('transactions')) {
+            customer.transactions = [];
+        }
+
+        customer.transactions.push(`${customer.firstName} ${customer.lastName} made deposit of ${amount}$!`);
+        customer.totalMoney += amount;
+        
+        return `${customer.totalMoney}$`;
+    }
 
     findCustomerById(id) {
         return this.allCustomers.find(c => c.personalId == id);
     }
 }
+
+let bank = new Bank('SoftUni Bank');
+
+console.log(bank.newCustomer({ firstName: 'Gii', lastName: 'Goo', personalId: 6233267 }));
+console.log(bank.newCustomer({ firstName: 'Bii', lastName: 'Boo', personalId: 4151596 }));
+
+// bank.depositMoney(6233267, 250);
+// console.log(bank.depositMoney(6233267, 250));
+// bank.depositMoney(4151596, 555);
+
+// console.log(bank.withdrawMoney(6233267, 125));
+
+// console.log(bank.customerInfo(6233267));
+
