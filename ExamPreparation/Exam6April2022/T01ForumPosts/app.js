3473ae0dfd23c748bcd985d9c3eb571d10889f24
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-06-20 14:40:41 +0300

    Update app.js

--- ExamPreparation/Exam6April2022/T01ForumPosts/app.js
+++ ExamPreparation/Exam6April2022/T01ForumPosts/app.js
@@ -1,20 +1,4 @@
 window.addEventListener("load", solve);
 
 function solve() {
-  const titleField = document.getElementById('post-title');
-  const categoryField = document.getElementById('post-category');
-  const contentField = document.getElementById('post-content');
-  const publishBtn = document.getElementById('publish-btn');
-  publishBtn.addEventListener('click', getInformation);
-
-
-
-    
-
-   
-  }
-
-
-
-
 }
