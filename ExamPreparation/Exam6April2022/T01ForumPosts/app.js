d366da3c34d11f9fd9493fabe2ec16002b87a608
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-06-20 14:40:18 +0300

    Update app.js

--- ExamPreparation/Exam6April2022/T01ForumPosts/app.js
+++ ExamPreparation/Exam6April2022/T01ForumPosts/app.js
@@ -15,19 +15,7 @@ function solve() {
     let elementToRemove = null;
 
 
-    const liItem = document.createElement('li');
-    liItem.classList.add('rpost');
-
-    const articleEl = document.createElement('article');
-
-    const titlePost = document.createElement('h4');
-    titlePost.textContent = titleField.value;
-
-    const categoryPost = document.createElement('p');
-    categoryPost.textContent = `Category: ${categoryField.value}`;
-
-    const contentPost = document.createElement('p');
-    contentPost.textContent = `Content: ${contentField.value}`;
+    
 
    
   }
