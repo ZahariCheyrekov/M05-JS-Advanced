4f77142673a84d7ff29a5b5339fa4ce77a4cd078
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-06-23 17:51:20 +0300

    Update rentCar.test.js

--- ExamPreparation/Exam13March2022/T03RentCar/rentCar.test.js
+++ ExamPreparation/Exam13March2022/T03RentCar/rentCar.test.js
@@ -3,13 +3,6 @@ const rentCar = require('./rentCar');
 
 describe('RentCar Tests', () => {
     describe('Tests should check budget for rentig a car', () => {
-      
-
-       
-
-        it('should throw error for not integer value for the budget', () => {
-            expect(() => rentCar.checkBudget(10, 10, 'a')).to.throw(Error);
-        });
 
         it('should throw error for not integer value for the costPerDay and not integer value for the days', () => {
             expect(() => rentCar.checkBudget('a', 'a', 10)).to.throw(Error);
