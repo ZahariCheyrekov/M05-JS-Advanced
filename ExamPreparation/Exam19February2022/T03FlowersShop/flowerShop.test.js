81c93f6e166fe03e0ecccf7c20f4e0107c200c60
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-06-24 20:46:40 +0300

    Update flowerShop.test.js

--- ExamPreparation/Exam19February2022/T03FlowersShop/flowerShop.test.js
+++ ExamPreparation/Exam19February2022/T03FlowersShop/flowerShop.test.js
@@ -39,9 +39,7 @@ describe('FlowerShop tests', () => {
     describe('Test should check sellFlowers function', () => {
      
 
-        it('should trow error for invalid space type different from number', () => {
-            expect(() => flowerShop.sellFlowers([], 'invalid')).to.throw(Error);
-        });
+      
 
       
 
