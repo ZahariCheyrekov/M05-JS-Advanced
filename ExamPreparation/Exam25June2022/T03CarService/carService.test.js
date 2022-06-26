0c7b183508c692e172a6401149a955c24c5f0ed8
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-26 21:01:21 +0300

    Add discount funtion tests for numberOfParts >2

--- ExamPreparation/Exam25June2022/T03CarService/carService.test.js
+++ ExamPreparation/Exam25June2022/T03CarService/carService.test.js
@@ -47,6 +47,35 @@ describe('CarService Tests', () => {
             expect(carService.discount(0, 10)).to.equal('You cannot apply a discount');
         });
 
+        it('should return that the discount was applied successfully for numberOfParts 3 and totalPrice 100', () => {
+            const savedMoney = (15 / 100) * 100;
+            expect(carService.discount(3, 100)).to.equal(`Discount applied! You saved ${savedMoney}$`);
+        });
+
+        it('should return that the discount was applied successfully for numberOfParts 4 and totalPrice 100', () => {
+            const savedMoney = (15 / 100) * 100;
+            expect(carService.discount(4, 100)).to.equal(`Discount applied! You saved ${savedMoney}$`);
+        });
+
+        it('should return that the discount was applied successfully for numberOfParts 6 and totalPrice 100', () => {
+            const savedMoney = (15 / 100) * 100;
+            expect(carService.discount(6, 100)).to.equal(`Discount applied! You saved ${savedMoney}$`);
+        });
+
+        it('should return that the discount was applied successfully for numberOfParts 7 and totalPrice 100', () => {
+            const savedMoney = (15 / 100) * 100;
+            expect(carService.discount(7, 100)).to.equal(`Discount applied! You saved ${savedMoney}$`);
+        });
+
+        it('should return that the discount was applied successfully for numberOfParts 8 and totalPrice 100', () => {
+            const savedMoney = (30 / 100) * 100;
+            expect(carService.discount(8, 100)).to.equal(`Discount applied! You saved ${savedMoney}$`);
+        });
+
+        it('should return that the discount was applied successfully for numberOfParts 9 and totalPrice 100', () => {
+            const savedMoney = (30 / 100) * 100;
+            expect(carService.discount(8, 100)).to.equal(`Discount applied! You saved ${savedMoney}$`);
+        });
     });
 
     describe('Tests should check partsToBuy task function', () => {
