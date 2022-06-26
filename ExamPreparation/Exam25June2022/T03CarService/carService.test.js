ee8d2b0e9eea0d5b7223560b3dc1640f9634c3cc
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-26 20:58:15 +0300

    Add test for carService with Engine value

--- ExamPreparation/Exam25June2022/T03CarService/carService.test.js
+++ ExamPreparation/Exam25June2022/T03CarService/carService.test.js
@@ -1,2 +1,22 @@
 const { expect } = require('chai');
 const carService = require('./carService.js');
+
+describe('CarService Tests', () => {
+    describe('Tests should check carService task function', () => {
+
+        it('should return that the issue of the car will cost more money by value Engine', () => {
+            expect(carService.isItExpensive('Engine'))
+                .to
+                .equal('The issue with the car is more severe and it will cost more money');
+        });
+
+    });
+
+    describe('Tests should check discount task function', () => {
+
+    });
+
+    describe('Tests should check partsToBuy task function', () => {
+
+    });
+});
