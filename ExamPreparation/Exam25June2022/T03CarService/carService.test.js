7867ee35bebdd6d7799e3e70dc6899cdf711d12c
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-26 21:00:06 +0300

    Add discount function data validation logic

--- ExamPreparation/Exam25June2022/T03CarService/carService.test.js
+++ ExamPreparation/Exam25June2022/T03CarService/carService.test.js
@@ -3,7 +3,6 @@ const carService = require('./carService.js');
 
 describe('CarService Tests', () => {
     describe('Tests should check carService task function', () => {
-
         it('should return that the issue of the car will cost more money by value Engine', () => {
             expect(carService.isItExpensive('Engine'))
                 .to
@@ -25,6 +24,19 @@ describe('CarService Tests', () => {
 
     describe('Tests should check discount task function', () => {
 
+        it('should throw error for invalid first param with value array', () => {
+            expect(() => carService.discount([], 10)).to.throw('Invalid input');
+        });
+
+        it('should throw error for invalid second param with value array', () => {
+            expect(() => carService.discount(10, [])).to.throw('Invalid input');
+        });
+
+        it('should throw error for invalid params with value array', () => {
+            expect(() => carService.discount([], [])).to.throw('Invalid input');
+        });
+
+      
     });
 
     describe('Tests should check partsToBuy task function', () => {
