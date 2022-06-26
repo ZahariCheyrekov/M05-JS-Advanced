5ceed98e00d1bd5a18fb88e8e820b2f9120ce3ea
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-26 20:52:37 +0300

    Add addGoal function logic

--- ExamPreparation/Exam25June2022/T02SmartHike/SmartHike.js
+++ ExamPreparation/Exam25June2022/T02SmartHike/SmartHike.js
@@ -7,6 +7,28 @@ class SmartHike {
         this.resources = 100;
     }
 
- 
+    addGoal(peak, altitude) {
+        let alreadyExistingName = false;
+
+        for (const key of Object.keys(this.goals)) {
+            if (key == peak) {
+                alreadyExistingName = true;
+                break;
+            }
+        }
+
+        if (alreadyExistingName) {
+            return `${peak} has already been added to your goals`;
+        }
+
+        this.goals[peak] = altitude;
+        return `You have successfully added a new goal - ${peak}`;
+    }
+
+    
+
+   
+
+    
 }
 
