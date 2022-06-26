ddda5f0acd30e9a91a97cd3968fe83f14d304481
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-26 20:52:18 +0300

    Add class constructor

---
+++ ExamPreparation/Exam25June2022/T02SmartHike/SmartHike.js
@@ -0,0 +1,12 @@
+class SmartHike {
+
+    constructor(username) {
+        this.username = username;
+        this.goals = {};
+        this.listOfHikes = [];
+        this.resources = 100;
+    }
+
+ 
+}
+
