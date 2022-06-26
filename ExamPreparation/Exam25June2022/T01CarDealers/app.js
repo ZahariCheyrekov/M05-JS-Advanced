191c6b35210977622793787cfba618816a8d1fcd
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-26 20:47:06 +0300

    Get fields from html

--- ExamPreparation/Exam25June2022/T01CarDealers/app.js
+++ ExamPreparation/Exam25June2022/T01CarDealers/app.js
@@ -1,5 +1,20 @@
 window.addEventListener("load", solve);
 
 function solve() {
+  const makeFieldInput = document.getElementById('make');
+  const modelFieldInput = document.getElementById('model');
+  const yearFieldInput = document.getElementById('year');
+  const fuelTypeInputOption = document.querySelector('#fuel');
+  const originalCostInputField = document.getElementById('original-cost');
+  const sellingPriceInputField = document.getElementById('selling-price');
+
+  const tbody = document.getElementById('table-body');
+
+  const carListData = document.querySelector('#cars-list');
+
+  const profitInfoData = document.querySelector('#profit');
+
+  const publishBtnForm = document.getElementById('publish');
+  publishBtnForm.addEventListener('click', getDataForm);
   
 }
